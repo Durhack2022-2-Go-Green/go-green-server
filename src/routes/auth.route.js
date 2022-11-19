47b7b091c5d13c06ab52f1d257d18be467a25f34
WillMW02 <williammw.wmw@gmail.com>
2022-11-19 19:23:38 +0000

    feat: add endpoint for current user info

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,7 +5,7 @@ import * as AuthController from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/', AuthController.login);
+router.post('/', AuthMiddleware, AuthController.login);
 
 router.get('/', AuthController.getCurrentUser);
 
