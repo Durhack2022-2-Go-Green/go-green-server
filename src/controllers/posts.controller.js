1e96f4eb58b8786215635c64ab2dfbab3aed3122
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-11-19 21:35:54 +0000

    fix: add post/comment takes authorId

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,6 +1,7 @@
 import { PostModel } from '../schemas/post.schema.js';
 
 import getError from '../lib/errorhandler.js';
+import { CommentModel } from '../schemas/comment.schema.js';
 
 export const getPosts = async (req, res, next) => {
 	const { userId } = req.query;
@@ -32,9 +33,11 @@ export const getPost = async (req, res, next) => {
 };
 
 export const createPost = async (req, res, next) => {
-	//TODO verify that req.body.author is the same as req.auth._id
-
-	const post = new PostModel(req.body);
+	// TODO: check this - should be authorized here
+	const post = new PostModel({
+		authorId: req.user.id,
+		...req.body}
+	);
 
 	post.save((err, dat) => {
 		if(err) return res.status(400).json({ error: getError(err) });
@@ -43,15 +46,20 @@ export const createPost = async (req, res, next) => {
 };
 
 export const addComment = async (req, res, next) => {
-	//TODO verify that req.body.author is the same as req.auth._id
-	
+	// TODO: check this - should be authorized here
+
 	const { id } = req.params;
 
 	const post = PostModel.findById(id);
 	if (post === undefined) {
 		res.status(404).json({ message: 'Post not found' });
 	} else {
-		post.comments.push(req.body);
+		// TODO: is this the right thing to push onto the array?
+		const comment = new CommentModel({
+			authorId: req.user.id,
+			...req.body
+		});
+		post.comments.push(comment);
 		post.save((err, dat) => {
 			if(err) return res.status(400).json({ error: getError(err) });
 			res.status(201).json({ message: 'Comment added', user: dat});
