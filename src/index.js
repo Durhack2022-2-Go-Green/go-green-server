5b2b2ec89034865da98d3da58ce42ef46282d532
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-11-19 15:25:06 +0000

    feat: winston logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import logger from './log.js';
+
 import './config/env.config.js'; // workaround for ES imports being hoisted, to import the env file before other modules
 import server from './app.js';
 
+logger.info('Server starting');
+
 server.listen(process.env.PORT??8080, () => {
-	console.log(`Listening on port ${process.env.PORT??8080}`);
+	logger.info(`Listening on port ${process.env.PORT??8080}`);
 });
---
+++ src/log.js
@@ -0,0 +1,29 @@
+import winston from 'winston';
+
+const { colorize, combine, json, timestamp, printf } = winston.format;
+
+const LOGGER_MAX_FILE_SIZE = 10000000;
+winston.level = 'info';
+winston.format = combine(
+    timestamp(),
+    json()
+);
+winston.add(new winston.transports.File({ filename: './logs/error.log', level: 'error', maxsize: LOGGER_MAX_FILE_SIZE}));
+winston.add(new winston.transports.File({ filename: './logs/latest.log', level: 'info', maxsize: LOGGER_MAX_FILE_SIZE}));
+
+
+if (process.env.NODE_ENV !== 'production') {
+    winston.add(new winston.transports.Console({
+        format: combine(
+            colorize(),
+            timestamp({
+                format: 'HH:mm:ss'
+            }),
+            printf((info) => {
+                return `[${info.timestamp}] [${info.level}]: ${info.message}`;
+            })
+        )
+    }));
+}
+
+export default winston;
