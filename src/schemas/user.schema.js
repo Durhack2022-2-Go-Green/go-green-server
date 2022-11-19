b7384eb6e78c21fa4aed4a958ce32107c5c0740d
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-11-19 17:20:49 +0000

    feat: user password store/check

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,9 +1,14 @@
 import { Schema, model } from 'mongoose';
+import { Image } from './image.schema.js';
+
+import { hashPassword, comparePassword } from './lib/crypto.js';
 
 export const User = new Schema({
 	username: {
 		type: String,
-		required: true
+		required: true,
+		require: 'Name is required',
+		unique: 'Username already exists'
 	},
 	password: {
 		type: String,
@@ -12,8 +17,59 @@ export const User = new Schema({
 	dateCreated: {
 		type: Date,
 		required: true
+	},
+	profilePicture: Image,
+	friends: {
+		type: Array, 
+		default: []
+	},
+	pendingInvites:{
+		type: Array,
+		default: []
+	},
+	pendingRequests:{
+		type: Array,
+		default: []
+	},
+	blockedUsers:{
+		type: Array,
+		default: []
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false
+	},
+	desc:{
+		type: String,
+		default: '',
+		max: 256
+	},
+	city:{
+		type: String,
+		default: '',
+		max: 50
+	},
+	from:{
+		type: String,
+		default: '',
+		max: 50
 	}
+}, {collection: 'users'}, { timestamps: true });
+
+User.pre('save', async function(next) {
+	let user = this;
+
+	if (!user.isModified('password')) return next();
+
+	user.password = await hashPassword(user.password);
+	next();
 });
+
+User.methods = {
+	authenticateUser: async function(plaintextPassword) {    //basic jw authentication
+		return await comparePassword(plaintextPassword, this.password);
+	}
+};
 
 const UserModel = model('User', User);
 
