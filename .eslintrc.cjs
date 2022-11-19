82770657afac4c68e355fd803a99d6a2d7ec6785
WillMW02 <williammw.wmw@gmail.com>
2022-11-19 14:32:11 +0000

    ci: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true,
+		'browser': true,
+		'jest': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'eol-last': 'error',
+		'no-var' : 'error'
+	}
+};
