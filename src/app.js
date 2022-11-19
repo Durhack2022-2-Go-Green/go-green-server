2db4f8a5c38e292cf2cc0962b74855561a799a1e
WillMW02 <williammw.wmw@gmail.com>
2022-11-19 15:12:50 +0000

    chore: add body parsing and header middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import bodyParser from 'body-parser';
 
 import ApiRouter from './routes/api.route.js';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
 
 app.use('/api/', ApiRouter);
 
